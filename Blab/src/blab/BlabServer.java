abef58f262d1e15737dd3e41abb93255c7caebf7
MalloryVal <31904471+MalloryVal@users.noreply.github.com>
2017-10-24 23:34:37 -0400

    Update BlabServer.java
    Heres the link i used:      https://way2java.com/networking/chat-program-two-way-communication/
    
    I'm not sure how the DataInputStream is working, but essentially we should put an if statement that checks to see if the input exists so then when it does not exist, we will be able to continuously send messages

--- Blab/src/blab/BlabServer.java
+++ Blab/src/blab/BlabServer.java
@@ -34,18 +34,32 @@ public class BlabServer extends Thread
 				Socket se= ss.accept();
 				while(true)
 				{
+						//DataInputStream din=new DataInputStream(se.getInputStream());
+						//System.out.println("Client: "+din.readUTF());
+					if((DataInputStream din=new DataInputStream(se.getInputStream())) != null)
+					{
 						DataInputStream din=new DataInputStream(se.getInputStream());
 						System.out.println("Client: "+din.readUTF());
-						
+					}
+							
 						DataOutputStream dout=new DataOutputStream(se.getOutputStream());
 						System.out.print("Server: ");
 						
 						BufferedReader bin=new BufferedReader(new InputStreamReader(System.in));
-						String chat=bin.readLine();
+						//String chat=bin.readLine();
+						
+						//So basically you check to see if the receiving line (what the client sends) != null
+						//and put the lines that read that inside of an if statement, that way it only tries to 
+						//read the file if the client actually sent something
+						if((chat = bin.readLine()) != null)
+						{
+							dout.writeUTF(chat);
+						}
+						
 						
 						//break on exit
 						if(chat.equals("EXIT")) break;
-						dout.writeUTF(chat);
+						//dout.writeUTF(chat);
 				}
 			}
 			catch(SocketTimeoutException s)
